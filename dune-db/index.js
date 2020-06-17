0b6da8b41eb3dadbae0d8096c1258e6d0755f209
Shherardd <Gerardwjones@gmail.com>
2020-06-16 21:58:05 -0500

    Ava Unit Testing

--- dune-db/index.js
+++ dune-db/index.js
@@ -3,8 +3,13 @@
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
+const defaults = require('defaults')
 
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect : 'sqlite'
+  })
+
   const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
